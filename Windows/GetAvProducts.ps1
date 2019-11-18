2da9688b6a0abd85935e1bd1a6e6aa3f054e27c4
Chris Kachigian <ckachigian@users.noreply.github.com>
2019-11-18 18:31:28 -0500

    Create GetAvProducts.ps1

---
+++ Windows/GetAvProducts.ps1
@@ -0,0 +1,9 @@
+﻿#This script is intended to display Name and state of the AntiVirus Product(s) installed on the system.
+
+$tools = @(Get-WmiObject -Namespace “root\SecurityCenter2” -Class AntiVirusProduct)
+
+foreach ($tool in $tools){
+    #Write-Output "Name: $($tool.displayName) | State: $($tool.productState)"
+    Write-Output "Name: $($tool.displayName)"
+    Write-Output "State: $($tool.productState)`n"
+}
