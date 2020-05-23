cd70feb13512326db097804aef021b8499335267
Chris Kachigian <ckachigian@users.noreply.github.com>
2020-05-23 00:26:19 -0400

    Update GetVolumeBitlockerStatus.ps1

--- Windows/GetVolumeBitlockerStatus.ps1
+++ Windows/GetVolumeBitlockerStatus.ps1
@@ -1,1 +1,7 @@
+$items = @(Get-BitLockerVolume)
 
+foreach ($item in $items){
+    #Write-Output "Mount Point: $($item.MountPoint) | Status: $($item.VolumeStatus)"
+    Write-Output "Mount Point: $($item.MountPoint)"
+    Write-Output "Status: $($item.VolumeStatus)`n"
+}
