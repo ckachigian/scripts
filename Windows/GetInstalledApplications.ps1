16695f459b9dafe59049dcf915f814b3e3785300
Chris Kachigian <ckachigian@users.noreply.github.com>
2020-05-23 01:36:08 -0400

    Update GetInstalledApplications.ps1

--- Windows/GetInstalledApplications.ps1
+++ Windows/GetInstalledApplications.ps1
@@ -1,1 +1,4 @@
+# Intended to show installed applications
+# Note to self: this way is slow.  Go back and replace with a registry based version or provide an option to use either.
 
+Get-WmiObject -Class Win32_Product
