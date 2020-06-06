047b80c97352a4e7a6a58c5dd3c972985d3ac0ca
Chris Kachigian <ckachigian@users.noreply.github.com>
2020-06-05 23:34:45 -0400

    changed to Get-CimInstance

--- Windows/GetSerialNumber.ps1
+++ Windows/GetSerialNumber.ps1
@@ -1,5 +1,5 @@
 #This script is intended to display only the Serial Number of the computer.
 #It returns a single value
 
-$os=(Get-WmiObject Win32_OperatingSystem)
-write-output $os.SerialNumber
+$os=(Get-CimInstance -ClassName Win32_OperatingSystem)
+write-output $os.SerialNumber 
