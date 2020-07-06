f9f533a3338a6850b1f0f364dd7214a1997b06dd
Chris Kachigian <ckachigian@users.noreply.github.com>
2020-07-06 18:04:26 -0400

    fixed output of array

--- Windows/GetRunningProcesses.ps1
+++ Windows/GetRunningProcesses.ps1
@@ -1,11 +1,11 @@
 #This script retrieves the running processes from a system
 # It has two types of output as an example
 
-$processes = Get-Process | Select-Object Name, Id, StartTime, WorkingSet, CPU, HandleCount, Path
+$processes = @(Get-Process | Select-Object Name, Id, StartTime, WorkingSet, CPU, HandleCount, Path)
 Write-Output "Output sample 1:\n"
 Write-Output $processes
 
 Write-Output "Output sample 2:\n"
 foreach ($process in $processes){
-    Write-Output "$process.Name|$process.Id|$process.StartTime|$process.WorkingSet|$process.CPU|$process.HandleCount|$process.Path`n"
+    Write-Output "$($process.Name)|$($process.Id)|$($process.StartTime)|$($process.WorkingSet)|$($process.CPU)|$($process.HandleCount)|$($process.Path)`n"
 }
