35ac862979b02f88ca46599881ca7bb0cd046656
Chris Kachigian <ckachigian@users.noreply.github.com>
2022-04-21 00:27:37 -0400

    Added alternative
    Added alternative way to get the hardware serial number

--- MacoS/GetSerialNumber.zsh
+++ MacoS/GetSerialNumber.zsh
@@ -2,5 +2,6 @@
 # Gets the devices Serial Number
 
 sn="$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')"
+# sn="$(system_profiler SPHardwareDataType | awk '/Hardware UUID/ { print $3; }')"
 
 echo $sn
