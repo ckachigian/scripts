bb8ff2240fa1b9a3484ef80dcd8b81b3f3aa319c
Chris Kachigian <ckachigian@users.noreply.github.com>
2022-04-21 00:32:26 -0400

    Added alternate command
    Added an alternate commend.

--- MacoS/GetProductName.zsh
+++ MacoS/GetProductName.zsh
@@ -2,5 +2,6 @@
 # Gets the devices product name
 
 productname="$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/product-name/{print $(NF-1)}')"
+# productname="$(system_profiler SPHardwareDataType | awk '/Model Identifier/ { print $3; }')"
 
 echo $productname
